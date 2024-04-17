26c28e9e59971b1b3210695e04597f1fc9f35ae2
sidhartha <sidharthakumaras@gmail.com>
2024-04-17 16:48:57 +0530

    1-variables

--- 01-variables.sh
+++ 01-variables.sh
@@ -3,8 +3,8 @@
 person1=siddu
 person2=seeta
 
- echo "$person1: Hey person2 how are you"
- echo "$person2: Hey person1 how are you"
+ echo "$person1: Hey $person2 how are you"
+ echo "$person2: Hey $person1 how are you"
 
 
 
