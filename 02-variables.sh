1d7dea942d76e8b1785ecb1ebb819bfbe7ec3785
sidhartha <sidharthakumaras@gmail.com>
2024-04-17 17:17:38 +0530

    read command

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # passing the values during run-time - one way
-person1=$1
+person1=$1 #arguments/parameters
 person2=$2
 
  echo "$person1: Hey $person2 how are you"
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Enter Username::"
+read USERNAME
+
+echo "Enter Password::"
+read -s PASSWORD
+
+echo "Username is : $USERNAME, Password is : $PASSWORD"
+
