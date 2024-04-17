721712e64001dcdc3afcd7777e0f83d894ff05f4
sidhartha <sidharthakumaras@gmail.com>
2024-04-17 20:19:17 +0530

    conditions

---
+++ 08-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NO=$1
+
+if [ $NO -gt 10 ]
+then
+    echo "number $NO is greater than 10"
+else
+    echo "number $NO is less than 10"
+fi
+
+echo "Exit status of previous command : $?"
