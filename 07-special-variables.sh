f5e3a4f552c550ca63b4436ef47f93207fb23083
sidhartha <sidharthakumaras@gmail.com>
2024-04-17 19:17:52 +0530

    special varaibles

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -7,3 +7,7 @@ echo "count how many parameters passed : $#"
 echo "script name is : $0"
 
 echo "exit status of previous command : $?"
+
+echo "current working directory : $PWD"
+
+echo "Home directory of current user : $HOME"
