aed6b42e22168ec9c38f6948e7ee38a92ba24add
sidhartha <sidharthakumaras@gmail.com>
2024-04-18 16:42:13 +0530

    colors

--- 11-logfiles.sh
+++ 11-logfiles.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 DATE=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f 1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f 1) # removing .sh in file name
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[30m"
+N="\e[0m"
 
 USER=$(id -u)
 
 if [ $USER -ne 0 ]
 then 
-    echo "Be a root user to install any package..."
+    echo " $R Be a root user to install any package... $N"
     exit 1 # manually stop without continue
 else 
     echo "Root user"
