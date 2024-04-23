9f7d7ef47cc6d16921ca714ed5dd76ae83594ab3
sidhartha <sidharthakumaras@gmail.com>
2024-04-23 19:57:57 +0530

    shell

--- 14-other-script.sh
+++ 14-other-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COURSE="Devops & AWS from other script"
+COURSE="Devops & AWS - from other script"
 
 echo "Variable executing from other script : $COURSE"
 echo "Process Id of current script: $$"
